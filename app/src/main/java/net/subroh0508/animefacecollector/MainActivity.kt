3ee430dd9a0acbaa270649643280de14ba76801a
takashi <tktktks10@gmail.com>
2018-08-03 02:00:56 +0900

    [add] Future of Facial Detection

--- app/src/main/java/net/subroh0508/animefacecollector/MainActivity.kt
+++ app/src/main/java/net/subroh0508/animefacecollector/MainActivity.kt
@@ -1,10 +1,19 @@
 package net.subroh0508.animefacecollector
 
+import android.content.Context
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
+import net.subroh0508.animefacecollector.util.FaceDetector
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
+    val detector by lazy { FaceDetector(setupCascadeFile())}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,6 +21,14 @@ class MainActivity : AppCompatActivity() {
 
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
+
+        detect_button.setOnClickListener{ _ ->
+            liner_layout.addView(ImageView(this).apply{
+                val bitmap = BitmapFactory.decodeResource(resources, R.drawable.face_detect_test)
+                val bitmapDetected = detector.detect(bitmap)
+                if (bitmap != null) setImageBitmap(bitmapDetected)
+            })
+        }
     }
 
     /**
@@ -25,5 +42,48 @@ class MainActivity : AppCompatActivity() {
         init {
             System.loadLibrary("native-lib")
         }
+    }
+
+    private fun setupCascadeFile(): File? {
+        val cascadeDir = getDir("cascade", Context.MODE_PRIVATE)
+        var cascadeFile: File? = null
+        var `is`: InputStream? = null
+        var os: FileOutputStream? = null
+        try {
+            cascadeFile = File(cascadeDir, "lbpcascade_frontalface.xml")
+            if (!cascadeFile!!.exists()) {
+                `is` = getResources().openRawResource(R.raw.lbpcascade_animeface)
+                os = FileOutputStream(cascadeFile)
+                val buffer = ByteArray(4096)
+                var readLen = 0
+                do {
+                    readLen = `is`!!.read(buffer)
+                    if (readLen == -1){
+                        break
+                    }
+                    os!!.write(buffer, 0, readLen)
+                } while(true)
+            }
+        } catch (e: IOException) {
+            return null
+        } finally {
+            if (`is` != null) {
+                try {
+                    `is`!!.close()
+                } catch (e: Exception) {
+                    // do nothing
+                }
+
+            }
+            if (os != null) {
+                try {
+                    os!!.close()
+                } catch (e: Exception) {
+                    // do nothing
+                }
+
+            }
+        }
+        return cascadeFile
     }
 }
--- app/src/main/java/net/subroh0508/animefacecollector/util/FaceDetector.kt
+++ app/src/main/java/net/subroh0508/animefacecollector/util/FaceDetector.kt
@@ -1,18 +1,23 @@
 package net.subroh0508.animefacecollector.util
 
+import android.graphics.Bitmap
 import android.util.Log
 import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
 import org.opencv.core.Mat
 import org.opencv.core.MatOfRect
+import org.opencv.core.Scalar
 import org.opencv.core.Size
-import org.opencv.imgcodecs.Imgcodecs.IMREAD_COLOR
-import org.opencv.imgcodecs.Imgcodecs.imread
+import org.opencv.imgproc.Imgproc
 import org.opencv.imgproc.Imgproc.*
 import org.opencv.objdetect.CascadeClassifier
+import java.io.File
 
-class FaceDetector(){
-    val CASCADE_FILE = "./lbpcascade_animeface.xml"
-    val detector by lazy { CascadeClassifier(CASCADE_FILE) }
+
+class FaceDetector(cascadeFile: File?){
+    val CASCADE_FILE = "lbpcascade_animeface.xml"
+    val detector by lazy { CascadeClassifier(cascadeFile?.absolutePath) }
+    val MIN_SIZE = Size(125.0, 125.0)
 
     init {
         if (!OpenCVLoader.initDebug()){
@@ -20,16 +25,25 @@ class FaceDetector(){
         }
     }
 
-    fun detect(fileName: String): IntArray{
-        val image = imread(fileName, IMREAD_COLOR)
-        var gray: Mat? = null
-        cvtColor(image, gray, COLOR_BGR2GRAY)
-        equalizeHist(gray, gray)
+    fun detect(image: Bitmap): Bitmap?{
+        var mat = Mat()
+        var originalMat = Mat()
 
-        var face = MatOfRect()
-        detector.detectMultiScale(gray, face, 1.1, 5, 0,
-                                  Size(75.0, 75.0), Size(2048.0, 2048.0))
+        Utils.bitmapToMat(image, originalMat)
+        Utils.bitmapToMat(image, mat)
+        cvtColor(mat, mat, COLOR_BGR2GRAY)
+        equalizeHist(mat, mat)
 
-        return face.toArray() as IntArray
+        var faces = MatOfRect()
+        detector.detectMultiScale(mat, faces, 1.1, 5, 0,
+                                  MIN_SIZE, Size(2048.0, 2048.0))
+
+        for (face in faces.toArray()){
+            Imgproc.rectangle(originalMat, face.tl(), face.br(), Scalar(0.0, 0.0, 255.0), 10)
+        }
+
+        var bitmap = Bitmap.createBitmap(image.width, image.height, Bitmap.Config.RGB_565)
+        Utils.matToBitmap(originalMat, bitmap)
+        return bitmap
     }
 }
